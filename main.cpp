b1cc99665dfb0576f67ef69d260a47adc9b99995
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-03-17 10:09:41 -0300

    [modify] Update on the main function
    - Using the new classes Base64Encoder and Base64Decoder to demonstrate
      the application;
    - Separating the encode and decode process just to have a clean example;

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 #include <vector>
 #include <string>
 
-#include "include/Base64.hpp"
+#include "include/Base64Encoder.hpp"
+#include "include/Base64Decoder.hpp"
+
+void encode();
+void decode();
 
 /*!
  * @brief Program main function
@@ -12,38 +16,77 @@
  * @returns The exit code
  */
 int main () {
-    Base64 base64;
+    encode();
+    decode();
+    return 0;
+}
+
+void encode() {
+    Base64Encoder encoder;
     std::string text;
 
     text = "A";
     std::cout << "Encoding: " << text << std::endl;
-    std::cout << "Encoded: " << base64.encode(text) << std::endl;
+    std::cout << "Encoded: " << encoder.encode(text) << std::endl;
     std::cout << std::endl;
     text.clear();
 
     text = "AB";
     std::cout << "Encoding: " << text << std::endl;
-    std::cout << "Encoded: " << base64.encode(text) << std::endl;
+    std::cout << "Encoded: " << encoder.encode(text) << std::endl;
     std::cout << std::endl;
     text.clear();
 
     text = "ABC";
     std::cout << "Encoding: " << text << std::endl;
-    std::cout << "Encoded: " << base64.encode(text) << std::endl;
+    std::cout << "Encoded: " << encoder.encode(text) << std::endl;
     std::cout << std::endl;
     text.clear();
 
     text = "ããã";
     std::cout << "Encoding: " << text << std::endl;
-    std::cout << "Encoded: " << base64.encode(text) << std::endl;
+    std::cout << "Encoded: " << encoder.encode(text) << std::endl;
     std::cout << std::endl;
     text.clear();
 
     text = "correct horse battery staple";
     std::cout << "Encoding: " << text << std::endl;
-    std::cout << "Encoded: " << base64.encode(text) << std::endl;
+    std::cout << "Encoded: " << encoder.encode(text) << std::endl;
+    std::cout << std::endl;
+    text.clear();
+}
+
+void decode() {
+    Base64Decoder decoder;
+    std::string text;
+
+    text = "QQ==";
+    std::cout << "Decoding: " << text << std::endl;
+    std::cout << "Decoded: " << decoder.decode(text) << std::endl;
     std::cout << std::endl;
     text.clear();
 
-    return 0;
+    text = "QUI=";
+    std::cout << "Decoding: " << text << std::endl;
+    std::cout << "Decoded: " << decoder.decode(text) << std::endl;
+    std::cout << std::endl;
+    text.clear();
+
+    text = "QUJD";
+    std::cout << "Decoding: " << text << std::endl;
+    std::cout << "Decoded: " << decoder.decode(text) << std::endl;
+    std::cout << std::endl;
+    text.clear();
+
+    text = "w6PDo8Oj";
+    std::cout << "Decoding: " << text << std::endl;
+    std::cout << "Decoded: " << decoder.decode(text) << std::endl;
+    std::cout << std::endl;
+    text.clear();
+
+    text = "Y29ycmVjdCBob3JzZSBiYXR0ZXJ5IHN0YXBsZQ==";
+    std::cout << "Decoding: " << text << std::endl;
+    std::cout << "Decoded: " << decoder.decode(text) << std::endl;
+    std::cout << std::endl;
+    text.clear();
 }
